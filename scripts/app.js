52af070938bb8ddd99e385ab1f3791dd9b8147b1
betty <makewithbetty@gmail.com>
2015-08-31 07:08:17 -0400

    Built html5-video-exercise from commit 6ad6dc4 on branch master

---
+++ scripts/app.js
@@ -0,0 +1,85 @@
+'use strict';
+var app = app || {};
+(function($) {
+
+    var _events = {};
+    _events = $(_events);
+
+    app.App = function(){
+        this.url = "../data/playlist.json";
+        this.$video = $('#video-container');
+        this.$playlist = $('#playlist');
+
+        this.event = _events;
+
+        this.data = false;
+        
+        this.fetch = function(){
+            var self = this;
+
+            $.ajax({
+                url: self.url,
+                success: function(data) {
+                    self.event.trigger('success', data);
+                }
+            })
+        };
+
+        this.appendVideoItem = function(){
+            var self = this;
+
+            _.forEach(self.data, function(val){
+                self.event.trigger('video:added', val);
+            });
+        };
+
+
+        this.renderVideoPlayer = function(video){
+            var self = this,
+                video = video.data.video,
+                template = _.template($('#player-template').html()),
+                playerHTML = template(video);
+
+            self.$video.html(playerHTML);
+        };
+
+        this.addVideo = function(data){
+            var self = this,
+                video = new app.Video(data);
+
+            this.videoCollection.push(video);
+
+            video.render();
+
+            self.$playlist.append(video.el);
+        };
+
+        this.createPlaylist = function(data) {
+            var playlist = new app.Playlist(data);
+
+            this.playlist = playlist;
+        };
+
+        this.initialize = function() {
+            var self = this;
+            this.fetch();
+
+            this.event.on('success', function(event, data) {
+                // On success, create playlist and render videoplayer
+                self.createPlaylist(data);
+                self.renderVideoPlayer(self.playlist.videoCollection[0]);
+            });
+
+            this.event.on('video:added', function(event, data) {
+                self.addVideo(data);
+            });
+
+            this.event.on('player:loadVideo', function(event, video) {
+                self.renderVideoPlayer(video);
+            });
+
+        };
+    };
+
+    
+})(jQuery);
---
+++ scripts/video.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var app = app || {};
+
+app.Video = function(data) {
+
+  this.event = $({});
+  this.data;
+  this.el;
+  this.template = _.template($('#video-item-template').html());
+
+
+
+  this.render = function() {
+    this.el = $(this.template(this.data));
+
+    this.event.trigger('video:rendered');
+    app.VideoApp.event.trigger('video:rendered');
+  };
+
+
+  this.registerEvents = function() {
+    var self = this;
+    this.el.on('click', function() {
+      app.VideoApp.event.trigger('player:loadVideo', self);
+      app.VideoApp.event.trigger('playlist:selectVideo', self);
+    });
+  };
+
+  this.initialize = function(data) {
+    if(!data) return false;
+
+    var self = this;
+
+    this.data = data;
+
+    this.event.on('video:rendered', function() {
+      self.registerEvents();
+    });
+
+  };
+  this.initialize(data);
+};
